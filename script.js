0a817deaa1de0fc0092672a158465e3481b29aff
eifesu <yessochrisa@gmail.com>
2021-07-24 11:27:51 +0000

    Added resizeable grid on button click

--- script.js
+++ script.js
@@ -2,20 +2,31 @@ const grid = document.querySelector('#grid');
 
 const clear = document.querySelector('#clear');
 clear.addEventListener('click',(e) => {
+    grid.textContent= '';
     const squares = document.querySelectorAll('.square');
     squares.forEach((el) => {
         el.setAttribute('style', 'background-color: white');
     })
+    const blocks = Number(prompt("How many squares per side of the new grid ?"));
+    grid.setAttribute('style', `
+    grid-template-columns: repeat(${blocks}, auto);
+    grid-template-rows: repeat(${blocks}, auto);
+    `);
+    drawGrid(blocks * blocks);
 })
 
-for (let i = 0; i < 256; i++) {  
-    const square = document.createElement('div');
-    square.setAttribute('class', 'square');
-    square.addEventListener('mouseover', (e) => {
-        e.target.setAttribute('style', 'background-color: black;');
-    });
-    square.addEventListener('mouseout', (e) => {
-        e.target.setAttribute('style', 'background-color: white; transition: 5s');
-    });
-    grid.append(square);
+function drawGrid(s) {
+    for (let i = 0; i < s ; i++) {  
+        const square = document.createElement('div');
+        square.setAttribute('class', 'square');
+        square.addEventListener('mouseover', (e) => {
+            e.target.setAttribute('style', 'background-color: black;');
+        });
+        square.addEventListener('mouseout', (e) => {
+            e.target.setAttribute('style', 'background-color: white; transition: 5s');
+        });
+        grid.append(square);
+    }
 }
+
+drawGrid(256)
